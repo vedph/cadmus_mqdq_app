765f69c205fe12e16a7f620e46659d2c36aa42d6
Myrmex <dfusi@hotmail.com>
2021-12-19 09:38:25 +0100

    Refactored for new shell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { User, GravatarService, Thesaurus, ThesaurusEntry } from '@myrmidon/cadmus-core';
-import { AuthService } from '@myrmidon/cadmus-api';
+import { Thesaurus, ThesaurusEntry } from '@myrmidon/cadmus-core';
 import { AppService, AppQuery } from '@myrmidon/cadmus-state';
+import { Router } from '@angular/router';
+import { take } from 'rxjs/operators';
+import {
+  AuthJwtService,
+  GravatarService,
+  User,
+} from '@myrmidon/auth-jwt-login';
 
 @Component({
-  selector: 'cadmus-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   public user?: User;
@@ -16,10 +22,11 @@ export class AppComponent implements OnInit {
   constructor(
     @Inject('itemBrowserKeys')
     private _itemBrowserKeys: { [key: string]: string },
-    private _authService: AuthService,
+    private _authService: AuthJwtService,
     private _gravatarService: GravatarService,
     private _appService: AppService,
-    private _appQuery: AppQuery
+    private _appQuery: AppQuery,
+    private _router: Router
   ) {}
 
   ngOnInit(): void {
@@ -46,7 +53,7 @@ export class AppComponent implements OnInit {
     return this._itemBrowserKeys[id] || id;
   }
 
-  public getGravatarUrl(email: string, size = 80): string {
+  public getGravatarUrl(email: string, size = 80): string | null {
     return this._gravatarService.buildGravatarUrl(email, size);
   }
 
@@ -54,6 +61,11 @@ export class AppComponent implements OnInit {
     if (!this.logged) {
       return;
     }
-    this._authService.logout();
+    this._authService
+      .logout()
+      .pipe(take(1))
+      .subscribe((_) => {
+        this._router.navigate(['/home']);
+      });
   }
 }
